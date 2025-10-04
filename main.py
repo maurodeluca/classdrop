b01741a115b09f03f980419dffcda100b3deecd1
Mauro De Luca <39085015+maurodeluca@users.noreply.github.com>
2025-10-04 19:27:03 +0100

    Mount static css

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from app.routes import files, student, professor
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="ClassDrop API", description="API for Class File Sharing.")
 
@@ -9,7 +10,11 @@ app.include_router(files.router)
 app.include_router(student.router)
 app.include_router(professor.router)
 
+# Mout static files
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 # Redirect root to /student
 @app.get("/")
 async def root():
     return RedirectResponse(url="/student")
+
